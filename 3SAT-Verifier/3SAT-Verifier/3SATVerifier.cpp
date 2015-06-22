9b882fd65b6650d7a97214ddba4f175ef84e6590
jiraky90 <dev@zagomattia.it>
2015-06-22 10:36:52 +0200

    First version of 3SATVerifier.cpp

---
+++ 3SAT-Verifier/3SAT-Verifier/3SATVerifier.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+
+using namespace std;
+
+string absolutePath = "D:\\Development\\NP\\VC-Verifier\\VC-Verifier\\resources\\";
+string istance = absolutePath + "instances\\istance_01.txt";
+string certificate = absolutePath + "certificates\\certificate_01.txt";
+ofstream outputFile(absolutePath + "output\\output_01.txt");
+
+
+int main()
+{
+
+	return 0;
+}
