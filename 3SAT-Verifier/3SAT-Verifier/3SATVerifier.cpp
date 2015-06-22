5c6b1b82928a9be24a02e422da26fb9f5a59159e
jiraky90 <dev@zagomattia.it>
2015-06-22 12:13:51 +0200

    3SAT Verifier finished. It will return 0 if the formula is satisfied by the certificate, 1 otherwise. In the output file 0 will indicate NOT satisfied, 1 satisfied.

--- 3SAT-Verifier/3SAT-Verifier/3SATVerifier.cpp
+++ 3SAT-Verifier/3SAT-Verifier/3SATVerifier.cpp
@@ -1,15 +1,59 @@
 #include <fstream>
+#include "../../includes/formula.h"
+#include <iostream>
+#include <string>
 
 using namespace std;
 
-string absolutePath = "D:\\Development\\NP\\VC-Verifier\\VC-Verifier\\resources\\";
-string istance = absolutePath + "instances\\istance_01.txt";
+string absolutePath = "D:\\Development\\NP\\3SAT-Verifier\\3SAT-Verifier\\resources\\";
+string istance = absolutePath + "instances\\instance_01.txt";
 string certificate = absolutePath + "certificates\\certificate_01.txt";
 ofstream outputFile(absolutePath + "output\\output_01.txt");
 
+int literals;
+int clauses;
+
+Formula * f;
 
 int main()
 {
+	ifstream inputFile(istance);
 
-	return 0;
+	if (inputFile.is_open())
+	{
+		inputFile >> literals;
+		inputFile >> clauses;
+
+		f = new Formula(clauses, literals);
+
+		for (int i = 0; i < clauses; ++i)
+		{
+			int l1;
+			int l2;
+			int l3;
+
+			inputFile >> l1;
+			inputFile >> l2;
+			inputFile >> l3;
+
+			f->addClause(abs(l1)-1, abs(l2)-1, abs(l3)-1, l1 > 0, l2 > 0, l3 > 0);
+		}
+	}
+
+	ifstream certFile(certificate);
+
+	if (certFile.is_open())
+	{
+		for (int i = 0; i < literals; ++i)
+		{
+			bool value;
+			certFile >> value;
+			f->SetAssignment(i, value);
+		}
+	}
+
+	bool issat = f->IsSatisfied();
+
+	outputFile << issat;
+	return !issat;
 }
---
+++ 3SAT-Verifier/3SAT-Verifier/resources/instances/README.txt
@@ -0,0 +1,8 @@
+Format example
+-----------------
+F = (a OR !b OR c) AND (-d OR e OR !f)
+
+instance.txt
+6 2				#literals = 6 #clauses = 2
+1 -2 3			clause #1
+-4 5 -6			clause #2
