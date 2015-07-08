5fa4d18a67507439fd5c9da39f06383f62edbace
Stefano Angeleri <weltall2@gmail.com>
2015-07-08 14:35:32 +0200

    Updated some examples and made vc verifier check for k

--- 3SAT-Generator/3SAT-Generator/3SATGenerator.cpp
+++ 3SAT-Generator/3SAT-Generator/3SATGenerator.cpp
@@ -13,11 +13,25 @@ using namespace std;
 
 int PA_Generate(std::ostream &instance, std::ostream &certificate, std::unordered_map<std::string, std::string> options)
 {
-	instance << "c test" << endl;
+	/*instance << "c test" << endl;
 	instance << "p cnf 6 2" << endl;
 	instance << "1 -2 3 0" << endl;
 	instance << "-4 5 -6 0" << endl;
 	certificate << "1 1 0 0 0 1" << endl;
+	*/
+
+	instance << "c test" << endl;
+	instance << "p cnf 3 7" << endl;
+	instance << "1 2 3 0" << endl;
+	instance << "1 2 -3 0" << endl;
+	instance << "1 -2 3 0" << endl;
+	instance << "-1 2 3 0" << endl;
+	instance << "1 -2 -3 0" << endl;
+	instance << "-1 2 -3 0" << endl;
+	instance << "-1 -2 3 0" << endl;
+	//instance << "-1 -2 -3 0" << endl;
+
+	certificate << "1 1 1" << endl;
 	return 0;
 }
 
