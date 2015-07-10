99e23595faf4f9a7ab801f38771982dd6f301105
jiraky90 <dev@zagomattia.it>
2015-07-10 15:18:59 +0200

    Removed unix "read".

--- final/server/main.cpp
+++ final/server/main.cpp
@@ -143,7 +143,7 @@ int main()
 
 	if (PA_Verifier(PA_InstanceIN_PostHardReduction, PA_CertificateIN_PostHardReduction) == PA_Verifier_Server(PA_InstanceIN_PostHardReduction_ForServer, PA_CertificateIN_PostHardReduction_ForServer))
 	{
-		points.reductionHard = 1;
+		points.reductionHard = 5;
 	}
 	
 	cout << "DONE" << endl;
@@ -151,7 +151,5 @@ int main()
 
 	#if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 		system("pause");
-	#else
-		system("read");
 	#endif
 }
